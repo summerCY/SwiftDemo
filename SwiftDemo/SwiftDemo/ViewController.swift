4e8937f7d0f4b6455216b2d386a182a57680aa1b
陈扬 <cyly@cylydemacbook-pro.local>
2018-03-26 13:49:56 +0800

    初始化

---
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by 咩咩咩 on 2018/3/26.
+//  Copyright © 2018年 cyly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
